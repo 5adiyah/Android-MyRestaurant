15a28b0aaeb721df907e0fc8a8501989e010ddf0
LaTaevia Berry and Sadio Ali <sadio@>
2016-06-28 09:46:55 -0700

    dd a rule to begin the RestaurantsActivity and define the mRestaurantListView before each test

---
+++ app/src/test/java/com/epicodus/myrestaurant/RestaurantsActivityTest.java
@@ -0,0 +1,27 @@
+package com.epicodus.myrestaurant;
+
+/**
+ * Created by Guest on 6/28/16.
+ */
+import android.os.Build;
+import android.widget.ListView;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+
+public class RestaurantsActivityTest {
+    private RestaurantsActivity activity;
+    private ListView mReataurantListView;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(RestaurantsActivity.class);
+        mReataurantListView = (ListView) activity.findViewById(R.id.listView);
+    }
+}
