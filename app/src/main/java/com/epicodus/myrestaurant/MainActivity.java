c04a8151527641e7bd239ce90d9a95fef3203ff2
LaTaevia Berry and Sadio Ali <lataevia+sadio@>
2016-06-28 16:41:59 -0700

    Final Commit of the day

--- app/src/main/java/com/epicodus/myrestaurant/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurant/MainActivity.java
@@ -13,7 +13,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  View.OnClickListener {
     @Bind(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
     @Bind(R.id.locationEditText) EditText mLocationEditText;
     @Bind(R.id.appNameTextView) TextView mAppNameTextView;
@@ -24,17 +24,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v){
-                    String location = mLocationEditText.getText().toString();
-                    Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
-                    intent.putExtra("location", location);
-                    startActivity(intent);
-                }
-            });
-
         Typeface restaurantFont = Typeface.createFromAsset(getAssets(), "fonts/AmaticSC-Regular.ttf");
         mAppNameTextView.setTypeface(restaurantFont);
+
+        mFindRestaurantsButton.setOnClickListener(this);
+    }
+
+        @Override
+        public void onClick(View v){
+            if(v == mFindRestaurantsButton) {
+                String location = mLocationEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                intent.putExtra("location", location);
+                startActivity(intent);
+            }
+        }
+
     }
 }
